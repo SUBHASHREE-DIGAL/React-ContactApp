eb9a3dd5ffd617a3ef551cd9eecc6dc3f56f8737
AKHI-1996 <akhilesh004123@gmail.com>
2022-09-10 15:24:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,56 +1,87 @@
-import React,{useEffect,useState}from 'react';
+import React from "react";
 import "./App.css";
-import {ImQuotesLeft  } from 'react-icons/im';
-import { CgQuote } from 'react-icons/cg';
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [news, setNews] = useState([]);
 
-   // 2nd
-const[quote,setQuote]=useState([])
+  useEffect(() => {
+    //  let data = async () => {
+    //    let response = await axios.get(
+    //      "https://newsapi.org/v2/top-headlines?country=in&apiKey=d3408808d49d462a99f21e8472eb00cc"
+    //    );
+    //    console.log(response.data.articles);
+    //    setNews(response.data.articles);
+    //  };
+    //  data();
+    fetchNews();
+  }, []);
 
-
-// first check the API data
-useEffect(()=>{
-let data= async () =>{
-let result=await axios.get('https://api.quotable.io/random')
-   console.log(result);
-   // 3rd
-setQuote(result.data)
-}
-data()
-},[])
-
-//    QUOTES GENERATOR
-let fetchData=async()=>{
-   let result=await axios.get('https://api.quotable.io/random')
-   setQuote(result.data)
-}
-
+  let fetchNews = async () => {
+    let response = await axios.get(
+      "https://newsapi.org/v2/top-headlines?country=in&apiKey=d3408808d49d462a99f21e8472eb00cc"
+    );
+    console.log(response.data.articles);
+    setNews(response.data.articles);
+  };
 
   return (
-    <div className='App'>
-      <div className='container text-center'>
-         <ImQuotesLeft/>
-      <h1>TRUST THE TIMING OF YOUR LIFE</h1>
-         <div className='row'>
-            <div className='col'>
-            <div className="card border-danger mt-5" >
-                  <div className="card-header text-center border-0"> Quotes </div>
-                  <div className="card-body text-center">
-                        <blockquote className="blockquote mb-0 text-danger fw-bolder">
-                                 <p><CgQuote/>{quote.content} <CgQuote/></p>
-                                  <footer className="blockquote-footer my-2"><cite title="Source Title">{quote.author}</cite></footer>
-                         </blockquote>
-                         <button onClick={fetchData} className='btn btn-sm btn-outline-danger '>New Quote</button>
+    <React.Fragment>
+      <header className="head">
+        <h1>News Articles</h1>
+        <div>
+          <button onClick={fetchNews} className="btn btn-outline-danger btn-sm">
+            Fetch news
+          </button>
+        </div>
+      </header>
+
+      <article>
+        <div className="container">
+          <div className="row">
+            {news.map((value) => {
+              return (
+                <div className="col-12 my-3" key={value.id}>
+                  <div className="card text-center">
+                    <img
+                      src={value.urlToImage}
+                      className="card-img-top"
+                      alt="/"
+                    />
+                    <div className="card-header border-0 bg-info">
+                      <h2 className="card-title">{value.title}</h2>{" "}
+                    </div>
+                    <div className="card-body">
+                      <h4 className="card-text">{value.content}</h4>
+                      <p className="card-text">{value.description}</p>
+                      <a
+                        className="btn btn-info"
+                        href={value.url}
+                        role="button"
+                      >
+                        Read more
+                      </a>
+                      <div className="my-2">
+                        {" "}
+                        <p className="card-text fw-bolder">
+                          {" "}
+                          {value.author} ,{" "}
+                          <small className="text-muted">
+                            {value.publishedAt}
+                          </small>
+                        </p>
+                      </div>
+                    </div>
                   </div>
-                  </div>
-            </div>
-         </div>
-      </div>
-      
-    </div>
-  )
-}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </article>
+    </React.Fragment>
+  );
+};
 
 export default App;
